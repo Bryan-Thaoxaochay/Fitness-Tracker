1af155d2b969c53cd760c06a5c8b6ea4313803b6
LittleMagician <bthaoxaochay@gmail.com>
2021-04-13 16:15:54 -0500

    Connected and added data to mongoDB

--- models/workout.js
+++ models/workout.js
@@ -2,39 +2,39 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const WorkoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now
     },
-    type: {
-        type: String,
-        trim: true,
-        required: 'Enter the exercise type (i.e. resistance, conditioning)'
-    },
-    name: {
-        type: String,
-        trim: true,
-        required: 'Enter the exercise name'
-    },
-    duration: {
-        type: Number,
-        required: 'Enter the length of the workout in minutes'
-    },
-    weight: {
-        type: Number,
-        required: 'Enter how much weight was lifted'
-    },
-    reps: {
-        type: Number,
-        required: 'Enter the number of reps'
-    },
-    sets: {
-        type: Number,
-        required: 'Enter the number of sets'
-    },
+    exercises: [
+        {        
+            type: {
+                type: String,
+                trim: true,
+                required: 'Enter the exercise type (i.e. resistance, conditioning)'
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: 'Enter the exercise name'
+            },
+            duration: {
+                type: Number,
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            }
+        }
+    ],
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
