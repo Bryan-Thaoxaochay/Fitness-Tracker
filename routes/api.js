dcb3eefcdcb2fafc7e74c4f1ccce99b476417f44
LittleMagician <bthaoxaochay@gmail.com>
2021-04-13 11:31:33 -0500

    Server is running

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+// Require model
+
+// Add exercises to recent workout plan
+
+// Add new exercises to new workout plan
+
+// View the combined weight of multiple exercises from the past seven workouts on the stats page
+
+// View the total duration of each workout from the past seven workouts on the stats page
+
+module.exports = router;
